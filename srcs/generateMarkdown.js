14d7a30322f8901a7e087d2792cc3ae462ca01d2
imran-rassi <imran.rassi.2020@gmail.com>
2022-10-29 23:36:32 -0400

    added functions to generatemarkdown.js

---
+++ srcs/generateMarkdown.js
@@ -0,0 +1,46 @@
+// processing the generateMarkdown //
+
+// If there is no license, return an empty string
+
+// added the function of licencebadge to show if the licence is MIT so appear the MIT licence badge.
+
+    function renderLicenseBadge(license) {
+      if (!license) {
+      return ``;
+      } else {
+      return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+      }
+    }
+
+// If there is no license, return an empty string
+// adding the link of the licence and if there is no license returning to an empty string because if there is no licence in our repo.
+
+    function renderLicenseLink(license) {
+      if (license === 'MIT') {
+        return `https://lbesson.mit-license.org/`
+      } else {
+         return ``;
+      }
+    }
+
+// If there is no license, return an empty string
+// adding the section of license give a message of This application is covered by the licence that we added
+// and if there is no license returning to an empty string.
+
+    function renderLicenseSection(license) {
+      if (!license) {
+        return ``;
+      } else {
+      return `## Licenses
+      This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+      }
+    }
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
